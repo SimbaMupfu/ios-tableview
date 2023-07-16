230e9b0d90fcf7837426ee1ccd9366ee8362f5f0
simba_mupfu <scmupfururirwa@gmail.com>
2023-07-16 22:50:17 +0200

    Create a demo app for a table view in iOS

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Simbarashe Mupfururirwa on 2023/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
